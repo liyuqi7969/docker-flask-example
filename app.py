7cb61535797c0a0a0a8095287a714ace9329781d
winner <liyuqi@gmail.com>
2018-03-06 17:22:30 +0800

    change html page

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask
-
+from flask import render_template
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello world! This is served by Flask in a Docker container!'
+    return render_template("index.html")
 
 
 if __name__ == '__main__':
